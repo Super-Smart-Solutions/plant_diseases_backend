c6a63ce8ddca5f382b9317d38270c70cb0218a19
ahmedelgamal0 <a.elgamal98@outlook.com>
2024-07-28 13:37:22 +0300

    Add generate docs pre-commit

---
+++ generate_json_api_doc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Start the FastAPI app using Docker Compose
+docker-compose -f deploy/docker-compose.yml -f deploy/docker-compose.dev.yml --project-directory . up --build -d
+
+# Wait for the app to start
+sleep 10
+
+# Download the OpenAPI JSON file
+curl -o api_doc.json http://0.0.0.0:8000/api/openapi.json
+
+# Stop the FastAPI app
+docker-compose -f deploy/docker-compose.yml -f deploy/docker-compose.dev.yml --project-directory . down
+
+# Install Redoc CLI
+npm install -g redoc-cli
+
+# Generate Redoc HTML
+mkdir -p docs
+redoc-cli bundle api_doc.json -o docs/index.html
